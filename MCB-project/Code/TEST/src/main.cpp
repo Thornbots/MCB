0ae14f51fe9f2725979b407004c6528244b380b7
rhit-teaneyje <teaneyje@rose-hulman.edu>
2023-03-17 20:26:39 -0400

    Code that lets us read from the IMU on type C

--- MCB-project/Code/TEST/src/main.cpp
+++ MCB-project/Code/TEST/src/main.cpp
@@ -1,32 +1,35 @@
 #include "tap/algorithms/smooth_pid.hpp"
 #include "tap/board/board.hpp"
-
+#include "params.h"
 #include "drivers_singleton.hpp"
 
 static constexpr tap::motor::MotorId MOTOR_ID = tap::motor::MOTOR1;
 static constexpr tap::can::CanBus CAN_BUS = tap::can::CanBus::CAN_BUS1;
-static constexpr int DESIRED_RPM = 3000;
+int DESIRED_RPM = 0;
 
 tap::arch::PeriodicMilliTimer sendMotorTimeout(2);
-tap::algorithms::SmoothPid pidController(20, 0, 0, 0, 8000, 1, 0, 1, 0);
+tap::arch::PeriodicMilliTimer periodicIMUUpdate(2);
+tap::algorithms::SmoothPid pidController = tap::algorithms::SmoothPid(PID_CONFIG);
+
 tap::motor::DjiMotor motor(src::DoNotUse_getDrivers(), MOTOR_ID, CAN_BUS, false, "cool motor");
 
 int main() {
     src::Drivers *drivers = src::DoNotUse_getDrivers();
-
     Board::initialize();
-
     drivers->can.initialize();
-
+    drivers->bmi088.initialize(500, 0.5, 0.1);
+    drivers->bmi088.requestRecalibration();
     motor.initialize();
 
-    while (1)
-    {
-        if (sendMotorTimeout.execute())
-        {
+    while (1) {
+        if(periodicIMUUpdate.execute()) {
+            drivers->bmi088.periodicIMUUpdate();
+            DESIRED_RPM = drivers->bmi088.getYaw();
+        }
+        if (sendMotorTimeout.execute()) {
             pidController.runControllerDerivateError(DESIRED_RPM - motor.getShaftRPM(), 1);
             motor.setDesiredOutput(static_cast<int32_t>(pidController.getOutput()));
-            drivers->djiMotorTxHandler.processCanSendData();
+            drivers->djiMotorTxHandler.encodeAndSendCanData();
         }
 
         drivers->canRxHandler.pollCanData();
