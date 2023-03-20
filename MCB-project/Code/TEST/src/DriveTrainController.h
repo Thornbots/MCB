9e90684998a947e984aa8c4d53d53aa2cadcac1b
rhit-teaneyje <teaneyje@rose-hulman.edu>
2023-03-20 13:50:55 -0400

    Refactored for Type-C and Unfactored

--- MCB-project/Code/TEST/src/DriveTrainController.h
+++ MCB-project/Code/TEST/src/DriveTrainController.h
@@ -1,41 +1,74 @@
 #pragma once
-#include "Core.h"
+#include "tap/algorithms/smooth_pid.hpp"
+#include "tap/board/board.hpp"
+#include "drivers_singleton.hpp"
+#include <cmath>
+#include <iostream>
+#include <string>
+#include "tap/architecture/periodic_timer.hpp"
+#include "tap/motor/dji_motor.hpp"
+#include "drivers_singleton.hpp"
+static tap::algorithms::SmoothPidConfig pid_conf_dt = { 20, 0, 0, 0, 8000, 1, 0, 1, 0, 0, 0 };
+
 
 namespace ThornBots {
-    class DrivetrainController {
+    class DriveTrainController {
     public:
-        DrivetrainController();
-        void Initialize();
-        void UpdateDrivetrain();
-        void SendMotorSpeeds();
-        void StopMotors();
-        int GetMotorSpeed(const char* motor_name);
-        bool SetMotorSpeed(int speed, const char* motor_name);
-        inline int GetTranslationSpeed() { return m_TranslationSpeed; }
-        inline int GetRotationSpeed() { return m_RotationSpeed; }
-        inline int GetBeybladingSpeed() { return m_BeybladingSpeed; }
-        inline void SetTranslationSpeed(int speed) { m_TranslationSpeed = speed; }
-        inline void SetRotationSpeed(int speed) { m_RotationSpeed = speed; }
-        inline void SetBeybladingSpeed(int speed) { m_BeybladingSpeed = speed; }
-    private:
-        bool KeyboardEnabled();
-        bool BeybladingEnabled(); 
-        int CalculateMotorSpeed(const char* motor_name);
-        int CalculateMotorSpeedWithKeyboard(const char* motor_name);
-        int CalculateMotorSpeedWithController(const char* motor_name);
-        int CalculateTranslatingSpeed(const char* motor_name);
-        float GetStickAngle(float xPosition, float yPosition);
-        float GetStickMagnitude(float xPosition, float yPosition);
-    private:
-        int m_FLSpeed, m_FRSpeed, m_BLSpeed, m_BRSpeed = 0; // motor speeds
-        int m_TranslationSpeed, m_RotationSpeed, m_BeybladingSpeed = 0; // other speeds
-        const int m_MaximumSpeed = 9500; // maximum speed a motor can reach
-        float m_BeybladeFactor = 0.5; // percentage from [0, 1] of how much power beyblading is allotted
+        DriveTrainController(tap::Drivers* driver);
+        ~DriveTrainController();
+        void setMotorValues(bool useWASD, bool doBeyblading, double right_stick_vert, double right_stick_horz, double left_stick_vert, double left_stick_horz, std::string input);
+        void setMotorSpeeds(bool sendMotorTimeout);
+        void stopMotors(bool sendMotorTimeout);
 
-        // drivetrain motors
-        tap::motor::DjiMotor* m_MotorFL;
-        tap::motor::DjiMotor* m_MotorFR;
-        tap::motor::DjiMotor* m_MotorBL;
-        tap::motor::DjiMotor* m_MotorBR;
+        
+    private:
+        //START getters and setters
+        inline int getMotorOneSpeed() { return motor_one_speed; }
+        inline int getMotorTwoSpeed() { return motor_two_speed; }
+        inline int getMotorThreeSpeed() { return motor_three_speed; }
+        inline int getMotorFourSpeed() { return motor_four_speed; }
+        inline void setMotorOneSpeed(int speed) { motor_one_speed = speed; }
+        inline void setMotorTwoSpeed(int speed) { motor_two_speed = speed; }
+        inline void setMotorThreeSpeed(int speed) { motor_three_speed = speed; }
+        inline void setMotorFourSpeed(int speed) { motor_four_speed = speed; }
+        inline int getTranslationSpeed() { return translation_speed; }
+        inline int getRotationSpeed() { return rotation_speed; }
+        inline int getBeybladingSpeed() { return beyblading_speed; }
+        inline void setTranslationSpeed(int speed) { translation_speed = speed; }
+        inline void setRotationSpeed(int speed) { rotation_speed = speed; }
+        inline void setBeybladingSpeed(int speed) { beyblading_speed = speed; }
+        inline int getMaxSpeed() { return max_speed; }
+        //STOP getters and setters
+
+        int getMotorOneSpeedWithWASD(bool doBeyblading);
+        int getMotorTwoSpeedWithWASD(bool doBeyblading);
+        int getMotorThreeSpeedWithWASD(bool doBeyblading);
+        int getMotorFourSpeedWithWASD(bool doBeyblading);
+        int getMotorOneSpeedWithCont(bool doBeyblading, double right_stick_vert, double right_stick_horz, double left_stick_vert, double left_stick_horz);
+        int getMotorTwoSpeedWithCont(bool doBeyblading, double right_stick_vert, double right_stick_horz, double left_stick_vert, double left_stick_horz);
+        int getMotorThreeSpeedWithCont(bool doBeyblading, double right_stick_vert, double right_stick_horz, double left_stick_vert, double left_stick_horz);
+        int getMotorFourSpeedWithCont(bool doBeyblading, double right_stick_vert, double right_stick_horz, double left_stick_vert, double left_stick_horz);
+        int getMotorSetOneTranslatingSpeed(double xPosition, double yPosition);
+        int getMotorSetTwoTranslatingSpeed(double xPosition, double yPosition);
+        double getAngle(double xPosition, double yPosition);
+        double getMagnitude(double xPosition, double yPosition);
+        double getScaledQuadratic(double magnitude);
+        
+        int motor_one_speed = 0; //Driver's front
+        int motor_two_speed = 0; //Passenger's front
+        int motor_three_speed = 0; //Driver's back
+        int motor_four_speed = 0; //Passenger's back
+        int translation_speed, rotation_speed, beyblading_speed = 0;
+        int max_speed = 9500; //The abs(maximum speed) we want the drivetrain motors to go to
+        double beyblading_factor = 0.5; //How much of the max_speed beyblading will eat up while robot is not translating range this from [0, 1]
+        static constexpr double PI = 3.14159; //Everyone likes Pi!
+        bool use_exponentional_controlling = true;
+        tap::Drivers *drivers;
+        tap::motor::DjiMotor motor_one = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "ID1", 0, 0);
+        tap::motor::DjiMotor motor_two = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR2, tap::can::CanBus::CAN_BUS1, false, "PURDON'T!", 0, 0);
+        tap::motor::DjiMotor motor_three = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR3, tap::can::CanBus::CAN_BUS1, false, "Put the possum in his room", 0, 0);
+        tap::motor::DjiMotor motor_four = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR4, tap::can::CanBus::CAN_BUS1, false, "Call 858-267-8107 for a good time!", 0, 0);
+        tap::algorithms::SmoothPid pidController = tap::algorithms::SmoothPid(pid_conf_dt);
+
     };
 }
--- MCB-project/Code/TEST/src/main.cpp
+++ MCB-project/Code/TEST/src/main.cpp
@@ -1,36 +1,129 @@
-#include "DrivetrainController.h"
+// General library includes
+#include <cmath>
+#include <iostream>
+#include <string>
+
+// Drivers
+#include "drivers_singleton.hpp"
+#include "drivers_singleton.hpp"
+#include "drivers_singleton.hpp"
+
+// Taproot
+#include "tap/architecture/periodic_timer.hpp"
+#include "tap/algorithms/smooth_pid.hpp"
+#include "tap/motor/dji_motor.hpp"
+#include "tap/board/board.hpp"
+
+//Our .h's
+#include "DriveTrainController.h"
 #include "TurretController.h"
 
-ThornBots::DrivetrainController s_DrivetrainController;
-ThornBots::TurretController s_TurretController;
+tap::arch::PeriodicMilliTimer sendMotorTimeout(2);
+tap::arch::PeriodicMicroTimer updateIMUTimeout(2);
+std::string WASDstring = ""; //Going to be the actual input string
+std::string controlString = ""; //Will be the last two chars in the "WASDstring" string. (What we're actually going to be looking at)
+src::Drivers *drivers;
+bool useWASD = false;
+bool doBeyblading = false;
+double right_stick_vert, right_stick_horz, left_stick_vert, left_stick_horz = 0.0;
+double angleOffset = 0.0;
 
-void InitializeRobot() {
-    INITIALIZE();
-    DRIVERS->remote.initialize();
-    s_DrivetrainController.Initialize();
-    s_TurretController.Initialize();
-}
 
-void UpdateRobot() {
-    s_DrivetrainController.UpdateDrivetrain();
-    s_TurretController.UpdateTurretController();
-    DRIVERS->canRxHandler.pollCanData();
-}
-
-void EmergencyStop() {
-    s_DrivetrainController.StopMotors();
-    s_TurretController.StopMotors();
+/**
+ * Used for reading the WASD keys on the keyboard. Updates two strings.
+ * WASDstring: A "queue" of characters for intrepreting WASD controls into robot movement
+ * controlString: The last (at most) two characters in the WASDString.
+*/
+void updateStrings() {
+    //START Reading WASD
+    if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::W)) {
+        if (WASDstring.find('w') == std::string::npos) {
+        WASDstring += 'w';
+        }
+    } else {
+        size_t pos = WASDstring.find('w');
+        if (pos != std::string::npos) {
+            WASDstring.erase(pos, 1);
+        }
+    }
+    if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::A)) {
+        if (WASDstring.find('a') == std::string::npos) {
+        WASDstring += 'a';
+        }
+    } else {
+        size_t pos = WASDstring.find('a');
+        if (pos != std::string::npos) {
+            WASDstring.erase(pos, 1);
+        }
+    }
+    if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::S)) {
+        if (WASDstring.find('s') == std::string::npos) {
+        WASDstring += 's';
+        }
+    } else {
+        size_t pos = WASDstring.find('s');
+        if (pos != std::string::npos) {
+            WASDstring.erase(pos, 1);
+        }
+    }
+    if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::D)) {
+        if (WASDstring.find('d') == std::string::npos) {
+        WASDstring += 'd';
+        }
+    } else {
+        size_t pos = WASDstring.find('d');
+        if (pos != std::string::npos) {
+            WASDstring.erase(pos, 1);
+        }
+    }
+    //STOP Reading WASD
+    //START Updating the substring
+    if (WASDstring.size() >= 2) {
+        controlString = WASDstring.substr(WASDstring.size() - 2);
+    } else {
+        controlString = WASDstring;
+    }
+    //STOP Updating the substring
 }
 
 int main() {
-    InitializeRobot();
+    src::Drivers *drivers = src::DoNotUse_getDrivers();
+    Board::initialize();
+    drivers->can.initialize();
+    drivers->remote.initialize();
+    drivers->bmi088.initialize(500, 0.5, 0.1);
+    drivers->bmi088.requestRecalibration();
+    ThornBots::DriveTrainController *driveTrainController = new ThornBots::DriveTrainController(drivers);
+    ThornBots::TurretController *turretController = new ThornBots::TurretController(drivers);
+    
     while (1) {
-        DELAY(10);
-        DRIVERS->remote.read();
-        if(DRIVERS->remote.isConnected()) {
-            UpdateRobot();
-        } else {
-            EmergencyStop();
+        drivers->remote.read(); //Reading the remote before we check if it is connected yet or not.
+        updateStrings();
+
+        if(updateIMUTimeout.execute()) {
+            drivers->bmi088.periodicIMUUpdate();
+            angleOffset = drivers->bmi088.getYaw(); //TODO: Make this calculate the AngleOffset and not the raw angle.
+        } //Stop reading from the IMU
+
+        if(drivers->remote.isConnected()) { //Doing stuff for the remote every loop
+            if(drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::LEFT_SWITCH) == tap::communication::serial::Remote::SwitchState::MID) { 
+                doBeyblading = false;
+      	    } else if((drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::LEFT_SWITCH) == tap::communication::serial::Remote::SwitchState::UP)) { 
+                doBeyblading = true;
+            } else if(drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::LEFT_SWITCH) == tap::communication::serial::Remote::SwitchState::DOWN) { 
+                //This is an undefined state as of now
+            }
+
+            //START the main code for the robot
+            driveTrainController->setMotorValues(useWASD, doBeyblading, right_stick_vert, right_stick_horz, left_stick_vert, left_stick_horz, controlString);
+            driveTrainController->setMotorSpeeds(sendMotorTimeout.execute());
+            turretController->setMotorValues(useWASD, doBeyblading, angleOffset, right_stick_vert, right_stick_horz);
+            turretController->setMotorSpeeds(sendMotorTimeout.execute());
+            //STOP the main code for the robot
+            
+        } else { //Remote not connected, so have everything turn off (Saftey features!)
+            driveTrainController->stopMotors(sendMotorTimeout.execute());
+            turretController->stopMotors(sendMotorTimeout.execute());
         }
     }
     return 0;
