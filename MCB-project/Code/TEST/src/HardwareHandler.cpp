1c72e917b94813fca3a704a3e137922e49e12577
rhit-teaneyje <teaneyje@rose-hulman.edu>
2023-04-23 16:35:05 -0400

    HardwareHandler hopefully done

--- MCB-project/Code/TEST/src/HardwareHandler.cpp
+++ MCB-project/Code/TEST/src/HardwareHandler.cpp
@@ -1,103 +1,141 @@
 #include "HardwareHandler.h"
 
 namespace ThornBots {
-            HardwareHandler::HardwareHandler() {
-                //START CAN1 Motors
-                tap::motor::DjiMotor tmp_motor_one = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[0][0] = &tmp_motor_one;
-                tap::motor::DjiMotor tmp_motor_two = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[0][1] = &tmp_motor_two;
-                tap::motor::DjiMotor tmp_motor_three = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[0][2] = &tmp_motor_three;
-                tap::motor::DjiMotor tmp_motor_four = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[0][3] = &tmp_motor_four;
-                tap::motor::DjiMotor tmp_motor_five = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[0][4] = &tmp_motor_five;
-                tap::motor::DjiMotor tmp_motor_six = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[0][5] = &tmp_motor_six;
-                tap::motor::DjiMotor tmp_motor_seven = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[0][6] = &tmp_motor_seven;
-                tap::motor::DjiMotor tmp_motor_eight = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[0][7] = &tmp_motor_eight;
-                //STOP CAN1 Motors
-                //START CAN2 Motors
-                tap::motor::DjiMotor tmp_motor_oneB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[1][0] = &tmp_motor_oneB;
-                tap::motor::DjiMotor tmp_motor_twoB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[1][1] = &tmp_motor_twoB;
-                tap::motor::DjiMotor tmp_motor_threeB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[1][2] = &tmp_motor_threeB;
-                tap::motor::DjiMotor tmp_motor_fourB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[1][3] = &tmp_motor_fourB;
-                tap::motor::DjiMotor tmp_motor_fiveB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[1][4] = &tmp_motor_fiveB;
-                tap::motor::DjiMotor tmp_motor_sixB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[1][5] = &tmp_motor_sixB;
-                tap::motor::DjiMotor tmp_motor_sevenB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[1][6] = &tmp_motor_sevenB;
-                tap::motor::DjiMotor tmp_motor_eightB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
-                MotorArray[1][7] = &tmp_motor_eightB;
-                //STOP CAN2 Motors   
-                           
-            }
+    HardwareHandler::HardwareHandler() {
+        //START CAN1 Motors
+        tap::motor::DjiMotor tmp_motor_one = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
+        MotorArray[0][0] = &tmp_motor_one;
+        tap::motor::DjiMotor tmp_motor_two = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR2, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
+        MotorArray[0][1] = &tmp_motor_two;
+        tap::motor::DjiMotor tmp_motor_three = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR3, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
+        MotorArray[0][2] = &tmp_motor_three;
+        tap::motor::DjiMotor tmp_motor_four = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR4, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
+        MotorArray[0][3] = &tmp_motor_four;
+        tap::motor::DjiMotor tmp_motor_five = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR5, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
+        MotorArray[0][4] = &tmp_motor_five;
+        tap::motor::DjiMotor tmp_motor_six = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR6, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
+        MotorArray[0][5] = &tmp_motor_six;
+        tap::motor::DjiMotor tmp_motor_seven = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR7, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
+        MotorArray[0][6] = &tmp_motor_seven;
+        tap::motor::DjiMotor tmp_motor_eight = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR8, tap::can::CanBus::CAN_BUS1, false, "pee pee poo poo", 0, 0);
+        MotorArray[0][7] = &tmp_motor_eight;
+        //STOP CAN1 Motors
+        //START CAN2 Motors
+        tap::motor::DjiMotor tmp_motor_oneB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS2, false, "pee pee poo poo", 0, 0);
+        MotorArray[1][0] = &tmp_motor_oneB;
+        tap::motor::DjiMotor tmp_motor_twoB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR2, tap::can::CanBus::CAN_BUS2, false, "pee pee poo poo", 0, 0);
+        MotorArray[1][1] = &tmp_motor_twoB;
+        tap::motor::DjiMotor tmp_motor_threeB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR3, tap::can::CanBus::CAN_BUS2, false, "pee pee poo poo", 0, 0);
+        MotorArray[1][2] = &tmp_motor_threeB;
+        tap::motor::DjiMotor tmp_motor_fourB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR4, tap::can::CanBus::CAN_BUS2, false, "pee pee poo poo", 0, 0);
+        MotorArray[1][3] = &tmp_motor_fourB;
+        tap::motor::DjiMotor tmp_motor_fiveB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR5, tap::can::CanBus::CAN_BUS2, false, "pee pee poo poo", 0, 0);
+        MotorArray[1][4] = &tmp_motor_fiveB;
+        tap::motor::DjiMotor tmp_motor_sixB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR6, tap::can::CanBus::CAN_BUS2, false, "pee pee poo poo", 0, 0);
+        MotorArray[1][5] = &tmp_motor_sixB;
+        tap::motor::DjiMotor tmp_motor_sevenB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR7, tap::can::CanBus::CAN_BUS2, false, "pee pee poo poo", 0, 0);
+        MotorArray[1][6] = &tmp_motor_sevenB;
+        tap::motor::DjiMotor tmp_motor_eightB = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR8, tap::can::CanBus::CAN_BUS2, false, "pee pee poo poo", 0, 0);
+        MotorArray[1][7] = &tmp_motor_eightB;
+        //STOP CAN2 Motors   
 
-            bool HardwareHandler::Initialize() {
-                //TODO
-                //drivers->bmi088.initialize(500, 0.0, 0.0);
-                CalibrateIMU();
-                //Board::initialize();
-                //drivers->remote.initialize();
-            }
+    }
 
-            void HardwareHandler::UpdateIMU() {
-                //TODO
-            }
+    int HardwareHandler::getRow(Motor MotorID) {
+        return (int) MotorID % 10 == 1 ? 1 : 0;
+    }
 
-            void HardwareHandler::CalibrateIMU() {
-                //TODO
-                //drivers->bmi088.requestRecalibration();
-            }
+    int HardwareHandler::getColumn(Motor MotorID) {
+        return (int) MotorID - 10 > 0 ? (int) MotorID - 10 : (int) MotorID;
+    }
 
-            void HardwareHandler::SetMotorPowerOutput(Motor motorID, int output) {
-                //TODO
-                // motor_one.setDesiredOutput(static_cast<int32_t>(pidController.getOutput()));
-            }
+    tap::motor::DjiMotor* HardwareHandler::getMotor(Motor MotorID) {
+        return this->MotorArray[getRow(MotorID)][getColumn(MotorID)];
+    }
 
-            void HardwareHandler::SetMotorRPMOutput(Motor motorID, int output) {
-                //TODO
-                //pidController.runControllerDerivateError(motor_one_speed - motor_one.getShaftRPM(), 1);
-                //motor_one.setDesiredOutput(static_cast<int32_t>(pidController.getOutput()));
+    bool HardwareHandler::Initialize() {
+        //TODO
+        //drivers->bmi088.initialize(500, 0.0, 0.0);
+        CalibrateIMU();
+        //Board::initialize();
+        //drivers->remote.initialize();
+        
+        //Initializing all of the motors (One that we aren't using (or don't even have plugged in) shouldn't affect anything outside of taking up a bit of extra memory)
+        for(int k = 0; k < sizeof(MotorArray), k++;) {
+            for(int j = 0; j < sizeof(MotorArray[0]), j++;) {
+                MotorArray[k][j]->initialize();
             }
+        }
 
-            void HardwareHandler::PollCanData() {
-                //TODO
-                //drivers->canRxHandler.pollCanData();
-            }
+    }
 
-            void HardwareHandler::SendCanData() {
-                //TODO
-                //drivers->djiMotorTxHandler.encodeAndSendCanData(); //Processes these motor speed changes into can signal
-            }
+    void HardwareHandler::UpdateIMU() {
+        drivers->bmi088.periodicIMUUpdate();
+    }
 
-            int32_t HardwareHandler::GetMotorShaftRPM(Motor motorID) {
-                //TODO
-                //motor_one.getShaftRPM()
-            }
+    void HardwareHandler::CalibrateIMU() {
+        drivers->bmi088.requestRecalibration();
+    }
 
-            bool HardwareHandler::GetIsDataSent() {
-                //TODO //Can we even see this? Taproot doesn't seem to have anything in the CanRxListener Class
-            }
- 
-            float HardwareHandler::GetMotorAngle(Motor motorID) {
-                //TODO
-            }
+    void HardwareHandler::SetMotorPowerOutput(Motor MotorID, int32_t Output) {
+        getMotor(MotorID)->setDesiredOutput(Output);
+    }
 
-            float HardwareHandler::GetIMUAngle() {
-                //TODO
-            }
+    void HardwareHandler::PollCanData() {
+        drivers->canRxHandler.pollCanData();
+    }
 
-            float HardwareHandler::GetMotorAngle(Motor motorID) {
-                //float position = tap::motor::DjiMotor::encoderToDegrees(motor_pitch.getEncoderWrapped());
-                //TODO: Make this only available for the 6020
-            }
+    void HardwareHandler::SendCanData() {
+        drivers->djiMotorTxHandler.encodeAndSendCanData(); //Processes these motor speed changes into can signal
+    }
+
+    int32_t HardwareHandler::GetMotorShaftRPM(Motor MotorID) {
+        return getMotor(MotorID)->getShaftRPM();
+    }
+
+    float HardwareHandler::GetIMUAngle(IMU_Radial Axis) {
+        switch(Axis) {
+            case(PITCH):
+                return drivers->bmi088.getPitch();
+                break;
+            case(YAW):
+                return drivers->bmi088.getYaw();
+                break;
+            case(ROLL):
+                return drivers->bmi088.getRoll();
+                break;
+        }
+    }
+
+    float HardwareHandler::GetIMUVelocity(IMU_Cardinal Axis) {
+        switch(Axis) {
+            case(X):
+                return drivers->bmi088.getGx();
+                break;
+            case(Y):
+                return drivers->bmi088.getGy();
+                break;
+            case(Z):
+                return drivers->bmi088.getGz();
+                break;
+        }
+    }
+
+    float HardwareHandler::GetIMUAcceleration(IMU_Cardinal Axis) {
+        switch(Axis) {
+            case(X):
+                return drivers->bmi088.getAx();
+                break;
+            case(Y):
+                return drivers->bmi088.getAy();
+                break;
+            case(Z):
+                return drivers->bmi088.getAz();
+                break;
+        }
+    }
+
+    float HardwareHandler::GetMotorAngle(Motor MotorID) {
+        return tap::motor::DjiMotor::encoderToDegrees(getMotor(MotorID)->getEncoderWrapped());               
+    }
 };
--- MCB-project/Code/TEST/src/HardwareHandler.h
+++ MCB-project/Code/TEST/src/HardwareHandler.h
@@ -10,7 +10,6 @@
 #include "tap/motor/dji_motor.hpp"
 #include "drivers_singleton.hpp"
 
-
 namespace ThornBots {
     class HardwareHandler {
         public:
@@ -21,38 +20,55 @@ namespace ThornBots {
             //STOP Variables
             //START Enuuums
             /**
-             * Enum for *all* of the motors on the robot (NOT SERVOS). The first digit is for which CAN bus the motor is on.
-             * The second digit is for which tap::motor::motorID the motor is on. (Relatively useless for )
+             * Enum for *all* of the motors on the robot (NOT SERVOS). The first digit is for which CAN bus the motor is on (MINUS ONE)).
+             * The second digit is for which tap::motor::motorID the motor is on (MINUS ONE).
             */
             enum Motor {
-                MOTOR_FRONT_LEFT = 11,
-                MOTOR_FRONT_RIGHT = 12,
-                MOTOR_BACK_LEFT = 13,
-                MOTOR_BACK_RIGHT = 14,
-                MOTOR_YAW = 17,
-                MOTOR_PITCH = 26,
-                MOTOR_INDEXER = 27,
-                FLYWHEEL_LEFT = 28,
-                FLYWHEEL_RIGHT = 25,
+                MOTOR_FRONT_LEFT = 00,
+                MOTOR_FRONT_RIGHT = 01,
+                MOTOR_BACK_LEFT = 02,
+                MOTOR_BACK_RIGHT = 03,
+                MOTOR_YAW = 06,
+                MOTOR_PITCH = 15,
+                MOTOR_INDEXER = 16,
+                FLYWHEEL_LEFT = 17,
+                FLYWHEEL_RIGHT = 14,
             };
+
+            enum IMU_Radial {
+                PITCH = 0,
+                YAW = 1,
+                ROLL = 2
+            };
+
+            enum IMU_Cardinal {
+                X = 0,
+                Y = 1,
+                Z = 2
+            };
+            
             //STOP Enuuums
             //START Methods
             bool Initialize();
             void UpdateIMU();
             void CalibrateIMU();
-            void SetMotorPowerOutput(Motor motorID, int Output);
-            void SetMotorRPMOutput(Motor motorID, int RPM);
+            void SetMotorPowerOutput(Motor MotorID, int32_t Output);
             void PollCanData();
             void SendCanData();
-            int32_t GetMotorShaftRPM(Motor motorID);
-            bool GetIsDataSent();
-            float GetMotorAngle(Motor motorID);
+            int32_t GetMotorShaftRPM(Motor MotorID);
+            float GetMotorAngle(Motor MotorID);
             float GetIMUAngle();
-            float GetMotorAngle(Motor motorID);
-            //STOP Method
+            float GetMotorAngle(Motor MotorID);
+            float GetIMUAngle(IMU_Radial Axis);
+            float GetIMUVelocity(IMU_Cardinal Axis);
+            float GetIMUAcceleration(IMU_Cardinal Axis);
+            //STOP Methods
 
         private:
             static constexpr int NUM_MOTORS = 8;
             tap::motor::DjiMotor* MotorArray[1][NUM_MOTORS - 1];
+            int getRow(Motor MotorID);
+            int getColumn(Motor MotorID);
+            tap::motor::DjiMotor* getMotor(Motor MotorID);
     };
 }
