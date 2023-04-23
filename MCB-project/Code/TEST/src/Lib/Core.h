b05b64239e1bc97d098270f7c06f305858d2cb0c
JHeflinger <jasonheflinger@gmail.com>
2023-04-23 16:33:52 -0400

    setup test robot

---
+++ MCB-project/Code/TEST/src/Lib/Core.h
@@ -0,0 +1,17 @@
+#include "tap/algorithms/smooth_pid.hpp"
+#include "tap/board/board.hpp"
+#include "../Taproot/drivers_singleton.hpp"
+#include "tap/architecture/periodic_timer.hpp"
+#include "tap/motor/dji_motor.hpp"
+#include <cmath>
+#include <iostream>
+#include <string>
+
+src::Drivers *drivers = src::DoNotUse_getDrivers();
+
+void initialize() {
+    Board::initialize();
+    drivers->can.initialize();
+    drivers->remote.initialize();
+    drivers->bmi088.initialize(500, 0.0, 0.0);
+}
