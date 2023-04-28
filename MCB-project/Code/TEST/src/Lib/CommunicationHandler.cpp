2720d93fae8206a3a006862383953608493e3e0f
nguyensl <nguyensl@rose-hulman.edu>
2023-04-28 19:46:34 -0400

    transferred communication handler

---
+++ MCB-project/Code/TEST/src/Lib/CommunicationHandler.cpp
@@ -0,0 +1,136 @@
+#include "CommunicationHandler.h"
+
+#define RX_BUFFER_LEN 128
+
+namespace ThornBots {
+
+    CommunicationHandler::CommunicationHandler() {
+        // initialization code for uart
+        drivers->uart.init<tap::communication::serial::Uart::UartPort::Uart1, 115200>();
+    }
+
+    bool CommunicationHandler::Initialize() {
+        drivers->remote.initialize();
+        isInitialized = true;
+        return isInitialized;
+    }
+
+    void CommunicationHandler::Update() {
+        drivers->remote.read();
+    }
+
+    uint8_t CommunicationHandler::GetSwitchState(const char switchID) {
+        switch (switchID)
+        {
+        case 'R':
+            return (uint8_t) drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::RIGHT_SWITCH);
+        case 'L':
+            return (uint8_t) drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::LEFT_SWITCH);
+        default:
+            return NULL;
+        }
+        
+    }
+
+    float CommunicationHandler::GetStickValue(const char* stickID) {
+        tap::communication::serial::Remote::Channel;
+        if (stickID[0] == 'R') {
+            switch (stickID[1])
+            {
+            case 'H':
+                drivers->remote.getChannel(tap::communication::serial::Remote::Channel::RIGHT_HORIZONTAL);
+            case 'V':
+                drivers->remote.getChannel(tap::communication::serial::Remote::Channel::RIGHT_VERTICAL);
+            default:
+                return NULL;
+            }
+        } else if (stickID[0] == 'L') {
+            switch (stickID[1])
+            {
+            case 'H':
+                drivers->remote.getChannel(tap::communication::serial::Remote::Channel::LEFT_HORIZONTAL);
+            case 'V':
+                drivers->remote.getChannel(tap::communication::serial::Remote::Channel::LEFT_VERTICAL);
+            default:
+                return NULL;
+            }
+        }
+        return NULL;
+     }
+
+    float CommunicationHandler::GetWheelValue() {
+        return drivers->remote.getWheel();
+    }
+
+    bool CommunicationHandler::GetIsInitialized() {
+        return isInitialized;
+    }
+
+    char* CommunicationHandler::GetKeysPressed() {
+        std::vector<char> charVector;
+
+        for (int i = 0; i < intToKey.size(); i++) {
+            if (drivers->remote.keyPressed(static_cast<tap::communication::serial::Remote::Key>(i))) {
+                charVector.push_back(intToKey.at(i));
+            };
+        }
+        
+        char returnArray[charVector.size()];
+        std::copy(charVector.begin(), charVector.end(), returnArray);
+
+        return returnArray;
+    }
+
+    bool CommunicationHandler::GetRightMouseClicked() {
+        return drivers->remote.getMouseR();
+    }
+
+    bool CommunicationHandler::GetLeftMouseClicked() {
+        return drivers->remote.getMouseL();
+    }
+
+    std::pair<int, int> CommunicationHandler::GetMouseCoords() {
+        int16_t xCoord = drivers->remote.getMouseX();
+        int16_t yCoord = drivers->remote.getMouseY();
+        return std::pair<int, int>(xCoord, yCoord);
+    }
+
+    char* CommunicationHandler::GetUartOutput() {
+        uint8_t readBuff[RX_BUFFER_LEN];
+        size_t read = drivers->uart.read(
+            tap::communication::serial::Uart::UartPort::Uart1,
+            &(readBuff[readBuffNumBytes]),
+            RX_BUFFER_LEN - readBuffNumBytes);
+
+        char *arr;
+
+        if (read > 0)
+        {
+            arr = new char[read];
+            for (size_t i = 0; i < read; i++)
+            {
+                arr[i] = readBuff[readBuffNumBytes + i];
+            }
+            readBuffNumBytes += read;
+        }
+        else
+        {
+            return NULL;
+        }
+        if (readBuffNumBytes >= RX_BUFFER_LEN)
+        {
+            readBuffNumBytes = 0;
+        }
+        return arr;
+    }
+
+    void CommunicationHandler::SendUart(const char* message) {
+        char* messageCopy;
+        std::memcpy(messageCopy, message, sizeof message);
+
+        drivers->uart.write(
+            tap::communication::serial::Uart::UartPort::Uart1,
+            reinterpret_cast<uint8_t *>(messageCopy),
+            0);
+    }
+};
--- MCB-project/Code/TEST/src/Lib/CommunicationHandler.h
+++ MCB-project/Code/TEST/src/Lib/CommunicationHandler.h
@@ -1,1 +1,73 @@
 #include "Core.h"
+
+namespace ThornBots {
+    class CommunicationHandler {
+        public:
+        CommunicationHandler();
+        ~CommunicationHandler() = default;
+
+        // Initializes remote
+        bool Initialize();                              
+
+        // Reads the remote, should be run every cycle
+        void Update();                                  
+
+        // Returns state of specified switch 
+        // Parameter should be either be 'R' or 'L'
+        uint8_t GetSwitchState(const char switchID);    
+
+        // Returns value of specified stick
+        // Parameter should be a character array where the 0th item is either 'R' or 'L' and the 1st item is either 'H' or 'V'
+        // e.g. {'R', 'H'} for right stick, horizontal axis
+        float GetStickValue(const char* stickID);       
+
+        // Returns value of specified wheel
+        float GetWheelValue();                    
+
+        // Returns whether or not the CommunicationHandler is initialized      
+        bool GetIsInitialized();       
+
+        // Returns a list of the keys being pressed
+        // Refer to the intToKey dictionary at the bottom of this header for available keys
+        // ^ is Ctrl    * is Shift       
+        char* GetKeysPressed();              
+
+        // Returns true if the RMB was clicked           
+        bool GetRightMouseClicked();              
+
+        // Returns true if the LMB was clicked      
+        bool GetLeftMouseClicked();      
+
+        // Returns an integer pair of the mouse's coordinates               
+        std::pair<int, int> GetMouseCoords();
+
+        // Returns a character array of Uart's output 
+        char* GetUartOutput();                      
+
+        // Sends messages to uart   
+        void SendUart(const char* message);             
+
+        private:
+        char* m_UartOutput;
+        size_t readBuffNumBytes = 0;
+        bool isInitialized = false;
+        std::map<int8_t, char> intToKey = {
+            {0, 'W'},
+            {1, 'S'},
+            {2, 'A'},
+            {3, 'D'},
+            {4, '*'},
+            {5, '^'},
+            {6, 'Q'},
+            {7, 'E'},
+            {8, 'R'},
+            {9, 'F'},
+            {10, 'G'},
+            {11, 'Z'},
+            {12, 'X'},
+            {13, 'C'},
+            {14, 'V'},
+            {15, 'B'}
+        };
+    };
+};
