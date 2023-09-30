dca364b853f2a2b75bee4b4731f772b54ef58863
Ryan Harmon <89997232+rhit-Dexterra@users.noreply.github.com>
2023-09-30 15:23:23 -0400

    main.cpp refactoring
    Reorganized the main.cpp file and cleaned up some code. Still need to implement KBM controls

--- MCB-project/Code/STANDARD/src/main.cpp
+++ MCB-project/Code/STANDARD/src/main.cpp
@@ -5,26 +5,26 @@
 
 // Drivers
 #include "drivers_singleton.hpp"
-#include "drivers_singleton.hpp"
-#include "drivers_singleton.hpp"
 
 // Taproot
-#include "tap/architecture/periodic_timer.hpp"
 #include "tap/algorithms/smooth_pid.hpp"
-#include "tap/motor/dji_motor.hpp"
+#include "tap/architecture/periodic_timer.hpp"
 #include "tap/board/board.hpp"
+#include "tap/motor/dji_motor.hpp"
 
-//Our .h's
+// Our .h's
 #include "DriveTrainController.h"
 #include "TurretController.h"
 
 tap::arch::PeriodicMilliTimer sendDrivetrainTimeout(2);
 tap::arch::PeriodicMilliTimer sendTurretTimeout(2);
 tap::arch::PeriodicMilliTimer updateIMUTimeout(2);
-std::string WASDstring = ""; //Going to be the actual input string
-std::string controlString = ""; //Will be the last two chars in the "WASDstring" string. (What we're actually going to be looking at)
+std::string WASDstring = "";     // Going to be the actual input string
+std::string controlString = "";  // Will be the last two chars in the "WASDstring" string. (What
+                                 // we're actually going to be looking at)
 src::Drivers *drivers;
-bool useWASD = false;
+bool KeyboardAndMouseEnabled = false;
+// bool useWASD = false;
 bool doBeyblading = false;
 double right_stick_vert, right_stick_horz, left_stick_vert, left_stick_horz = 0.0;
 int16_t wheel_value = 0;
@@ -36,173 +36,257 @@ bool turretIndependent = false;
 int rightSwitchValue = 1;
 int leftSwitchValue = 0;
 
-
 /**
  * Used for reading the WASD keys on the keyboard. Updates two strings.
  * WASDstring: A "queue" of characters for intrepreting WASD controls into robot movement
  * controlString: The last (at most) two characters in the WASDString.
-*/
-void updateStrings() {
-    //START Reading WASD
-    if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::W)) {
-        if (WASDstring.find('w') == std::string::npos) {
-        WASDstring += 'w';
+ */
+void updateStrings()
+{
+    // START Reading WASD
+    if (drivers->remote.keyPressed(tap::communication::serial::Remote::Key::W))
+    {
+        if (WASDstring.find('w') == std::string::npos)
+        {
+            WASDstring += 'w';
         }
-    } else {
+    }
+    else
+    {
         size_t pos = WASDstring.find('w');
-        if (pos != std::string::npos) {
+        if (pos != std::string::npos)
+        {
             WASDstring.erase(pos, 1);
         }
     }
-    if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::A)) {
-        if (WASDstring.find('a') == std::string::npos) {
-        WASDstring += 'a';
+    if (drivers->remote.keyPressed(tap::communication::serial::Remote::Key::A))
+    {
+        if (WASDstring.find('a') == std::string::npos)
+        {
+            WASDstring += 'a';
         }
-    } else {
+    }
+    else
+    {
         size_t pos = WASDstring.find('a');
-        if (pos != std::string::npos) {
+        if (pos != std::string::npos)
+        {
             WASDstring.erase(pos, 1);
         }
     }
-    if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::S)) {
-        if (WASDstring.find('s') == std::string::npos) {
-        WASDstring += 's';
+    if (drivers->remote.keyPressed(tap::communication::serial::Remote::Key::S))
+    {
+        if (WASDstring.find('s') == std::string::npos)
+        {
+            WASDstring += 's';
         }
-    } else {
+    }
+    else
+    {
         size_t pos = WASDstring.find('s');
-        if (pos != std::string::npos) {
+        if (pos != std::string::npos)
+        {
             WASDstring.erase(pos, 1);
         }
     }
-    if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::D)) {
-        if (WASDstring.find('d') == std::string::npos) {
-        WASDstring += 'd';
+    if (drivers->remote.keyPressed(tap::communication::serial::Remote::Key::D))
+    {
+        if (WASDstring.find('d') == std::string::npos)
+        {
+            WASDstring += 'd';
         }
-    } else {
+    }
+    else
+    {
         size_t pos = WASDstring.find('d');
-        if (pos != std::string::npos) {
+        if (pos != std::string::npos)
+        {
             WASDstring.erase(pos, 1);
         }
     }
-    //STOP Reading WASD
-    //START Updating the substring
-    if (WASDstring.size() >= 2) {
+    // STOP Reading WASD
+    // START Updating the substring
+    if (WASDstring.size() >= 2)
+    {
         controlString = WASDstring.substr(WASDstring.size() - 2);
-    } else {
+    }
+    else
+    {
         controlString = WASDstring;
     }
-    //STOP Updating the substring
+    // STOP Updating the substring
 }
 
 /**
  * Reads inputs from the mouse and handles what do do with that information
  * This function assumes that we are already in WASD mode (useWASD == true)
-*/
-void micky() {
-    
-    return;
-}
+ */
+void micky() { return; }
 
 /**
- * Reads inputs from the keyboard and mouse and habdkes what do do with that information
-*/
-void readKeyboardAndMicky() {
-    useWASD = false;
-    if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::CTRL)) {
-        if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::SHIFT)) {
-            if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::R)) {
-                useWASD = true; //Toggling between keyboard/mouse and controller input
-            } //End R
-        } //End shift
-    } //End CTRL
-
-    if(useWASD) {
-        if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::Q)) {
-            //TODO: Make this rotate the robot to the left (CCW)
-        } else if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::E)) {
-            //TODO: Make this rotate the robot to the right (CW)
-        }
-        if(drivers->remote.keyPressed(tap::communication::serial::Remote::Key::F)) {
-            doBeyblading = true;
-        } else { doBeyblading = false; }
-        micky();
-    } else {
-        //we are not using WASD. So pee pee poo poo. Enjoy the crap that is my code
+ * Reads inputs from the keyboard and mouse and checks to see if KBM(keyboard and Mouse) mode should
+ * be enabled or not. It requires the pressing of CTRL + SHIFT + R to enable KBM mode.
+ */
+void toggleKeyboardAndMouse()
+{
+    if (drivers->remote.keyPressed(tap::communication::serial::Remote::Key::CTRL) &&
+        drivers->remote.keyPressed(tap::communication::serial::Remote::Key::SHIFT) &&
+        drivers->remote.keyPressed(tap::communication::serial::Remote::Key::R))
+    {
+        KeyboardAndMouseEnabled = true;  // Toggling between keyboard/mouse and controller input
     }
     return;
 }
 
-int main() {
+int main()
+{
     src::Drivers *drivers = src::DoNotUse_getDrivers();
     Board::initialize();
     drivers->can.initialize();
     drivers->remote.initialize();
     drivers->bmi088.initialize(500, 0.0, 0.0);
     drivers->bmi088.requestRecalibration();
-    ThornBots::DriveTrainController *driveTrainController = new ThornBots::DriveTrainController(drivers);
+    ThornBots::DriveTrainController *driveTrainController =
+        new ThornBots::DriveTrainController(drivers);
     ThornBots::TurretController *turretController = new ThornBots::TurretController(drivers);
-    
-    while (1) {
+
+    while (1)
+    {
         modm::delay_us(10);
         drivers->canRxHandler.pollCanData();
-        drivers->remote.read(); //Reading the remote before we check if it is connected yet or not.
+        drivers->remote.read();  // Reading the remote before we check if it is connected yet or
+                                 // not.
 
-        if(updateIMUTimeout.execute()) {
+        if (updateIMUTimeout.execute())
+        {
             drivers->bmi088.periodicIMUUpdate();
-            angleOffset = drivers->bmi088.getYaw(); //TODO: Make this calculate the AngleOffset and not the raw angle.
-        } //Stop reading from the IMU
+            angleOffset =
+                drivers->bmi088
+                    .getYaw();  // TODO: Make this calculate the AngleOffset and not the raw angle.
+        }                       // Stop reading from the IMU
 
-        if(drivers->remote.isConnected()) { //Doing stuff for the remote every loop
-            updateStrings();
-            readKeyboardAndMicky();
-            if(drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::LEFT_SWITCH) == tap::communication::serial::Remote::SwitchState::MID) { 
-                isLeftStickUp = false;
-                isLeftStickDown = false;
-                doBeyblading = false;
-                turretIndependent = true;
-                leftSwitchValue = 1;
-      	    } else if((drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::LEFT_SWITCH) == tap::communication::serial::Remote::SwitchState::UP)) { 
-                isLeftStickDown = false;
-                isLeftStickUp = true;
-                doBeyblading = true;
-                turretIndependent = false;
-                leftSwitchValue = 2;
-            } else if(drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::LEFT_SWITCH) == tap::communication::serial::Remote::SwitchState::DOWN) { 
-                isLeftStickDown = true;
-                isLeftStickUp = false;
-                doBeyblading = false;
-                turretIndependent = false;
-                leftSwitchValue = 0;
+        if (drivers->remote.isConnected())
+        {  // If the remote is On and connected do the following
+
+            toggleKeyboardAndMouse();  // Check to see if KBM mode should be enabled or not
+            // updateStrings();             //Needs to be fixed
+
+            if (KeyboardAndMouseEnabled)
+            {
+                // We are using Keyboard and Mouse controls
+                // TODO : Make this work
             }
-            if(drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::RIGHT_SWITCH) == tap::communication::serial::Remote::SwitchState::MID) { 
-                //TODO: Make the drivebase allign with the turret. I.e. if the anglfoffset is negative make it spin CW or vice versus
-                isRightStickMid = true;
-                rightSwitchValue = 1;
-      	    } else if((drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::RIGHT_SWITCH) == tap::communication::serial::Remote::SwitchState::UP)) { 
-                //Nothing as of now
-                isRightStickMid = false;
-                rightSwitchValue = 2;
-            } else if(drivers->remote.getSwitch(tap::communication::serial::Remote::Switch::RIGHT_SWITCH) == tap::communication::serial::Remote::SwitchState::DOWN) { 
-                turretController->reZero();
-                isRightStickMid = false;
-                rightSwitchValue = 0;
-                //TODO: Lock the turret to the front of the drivetrain. I.e, set the desired angle to be 0. (The center of the robot)
+            else
+            {
+                // We are using the controller
+
+                // Get Current state of Left Switch on the remote and set the appropriate variables
+                auto leftSwitchState = drivers->remote.getSwitch(
+                    tap::communication::serial::Remote::Switch::LEFT_SWITCH);
+                switch (leftSwitchState)
+                {
+                    case tap::communication::serial::Remote::SwitchState::UP:
+                        isLeftStickDown = false;
+                        isLeftStickUp = true;
+                        doBeyblading = true;
+                        turretIndependent = false;
+                        leftSwitchValue = 2;
+                        break;
+
+                    case tap::communication::serial::Remote::SwitchState::MID:
+                        isLeftStickUp = false;
+                        isLeftStickDown = false;
+                        doBeyblading = false;
+                        turretIndependent = true;
+                        leftSwitchValue = 1;
+                        break;
+
+                    case tap::communication::serial::Remote::SwitchState::DOWN:
+                        isLeftStickDown = true;
+                        isLeftStickUp = false;
+                        doBeyblading = false;
+                        turretIndependent = false;
+                        leftSwitchValue = 0;
+                        break;
+                }
+
+                // Get Current state of Right Switch on the remote and set the appropriate variables
+                auto rightSwitchState = drivers->remote.getSwitch(
+                    tap::communication::serial::Remote::Switch::RIGHT_SWITCH);
+                switch (rightSwitchState)
+                {
+                    case tap::communication::serial::Remote::SwitchState::MID:
+                        // TODO: Make the drivebase align with the turret.
+                        // i.e., if the angle offset is negative, make it spin CW or vice versa
+                        isRightStickMid = true;
+                        rightSwitchValue = 1;
+                        break;
+
+                    case tap::communication::serial::Remote::SwitchState::UP:
+                        // Nothing as of now
+                        isRightStickMid = false;
+                        rightSwitchValue = 2;
+                        break;
+
+                    case tap::communication::serial::Remote::SwitchState::DOWN:
+                        turretController->reZero();
+                        isRightStickMid = false;
+                        rightSwitchValue = 0;
+                        // TODO: Lock the turret to the front of the drivetrain.
+                        // i.e., set the desired angle to be 0 (the center of the robot)
+                        break;
+                }
+
+                // Get Current state of the Right Stick on the remote and set the appropriate
+                right_stick_vert = drivers->remote.getChannel(
+                    tap::communication::serial::Remote::Channel::RIGHT_VERTICAL);
+                right_stick_horz = drivers->remote.getChannel(
+                    tap::communication::serial::Remote::Channel::RIGHT_HORIZONTAL);
+
+                // Get Current state of the Left Stick on the remote and set the appropriate
+                left_stick_vert = drivers->remote.getChannel(
+                    tap::communication::serial::Remote::Channel::LEFT_VERTICAL);
+                left_stick_horz = drivers->remote.getChannel(
+                    tap::communication::serial::Remote::Channel::LEFT_HORIZONTAL);
+
+                // Get Current state of the wheel on the remote and set the appropriate
+                wheel_value = drivers->remote.getWheel();
+
+                // Call the setMotorValues and setMotorSpeeds function in the DriveTrainController
+                // class
+                driveTrainController->setMotorValues(
+                    KeyboardAndMouseEnabled,
+                    doBeyblading,
+                    right_stick_vert,
+                    right_stick_horz,
+                    left_stick_vert,
+                    left_stick_horz,
+                    controlString,
+                    turretController->getYawEncoderAngle(),
+                    isRightStickMid,
+                    rightSwitchValue,
+                    leftSwitchValue);
+                driveTrainController->setMotorSpeeds(sendDrivetrainTimeout.execute());
+
+                // Call the setMotorValues and setMotor Speeds function in the TurretController class
+                turretController->setMotorValues(
+                    KeyboardAndMouseEnabled,
+                    doBeyblading,
+                    angleOffset,
+                    -right_stick_vert,
+                    right_stick_horz,
+                    driveTrainController->motor_one.getShaftRPM(),
+                    driveTrainController->motor_four.getShaftRPM(),
+                    wheel_value,
+                    isRightStickMid,
+                    isLeftStickUp,
+                    rightSwitchValue,
+                    leftSwitchValue);
+                turretController->setMotorSpeeds(sendTurretTimeout.execute());
             }
-
-            right_stick_vert = drivers->remote.getChannel(tap::communication::serial::Remote::Channel::RIGHT_VERTICAL);
-            right_stick_horz = drivers->remote.getChannel(tap::communication::serial::Remote::Channel::RIGHT_HORIZONTAL);
-            left_stick_vert = drivers->remote.getChannel(tap::communication::serial::Remote::Channel::LEFT_VERTICAL);
-            left_stick_horz = drivers->remote.getChannel(tap::communication::serial::Remote::Channel::LEFT_HORIZONTAL);
-            wheel_value = drivers->remote.getWheel();
-
-            driveTrainController->setMotorValues(useWASD, doBeyblading, right_stick_vert, right_stick_horz, left_stick_vert, left_stick_horz, controlString, turretController->getYawEncoderAngle(), isRightStickMid, rightSwitchValue, leftSwitchValue);
-            driveTrainController->setMotorSpeeds(sendDrivetrainTimeout.execute());
-            turretController->setMotorValues(useWASD, doBeyblading, angleOffset, -right_stick_vert, right_stick_horz, driveTrainController->motor_one.getShaftRPM(), driveTrainController->motor_four.getShaftRPM(), wheel_value, isRightStickMid, isLeftStickUp, rightSwitchValue, leftSwitchValue);
-            turretController->setMotorSpeeds(sendTurretTimeout.execute()); 
-            // drivers->djiMotorTxHandler.encodeAndSendCanData(); //Processes these motor speed changes into can signal
-           
-
-        } else { //Remote not connected, so have everything turn off (Saftey features!)
+        }
+        else
+        {  // Remote not connected, so have everything turn off (Saftey features!)
             driveTrainController->stopMotors(sendDrivetrainTimeout.execute());
             turretController->stopMotors(sendTurretTimeout.execute());
         }
