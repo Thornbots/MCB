b5a1bb9a0695bb4fe5d1731c4039472bf484fb9b
Ethan_Harden <hardenej@rose-hulman.edu>
2023-10-22 14:57:45 -0400

    work on indexer

--- MCB-project/Code/HERO/src/main.cpp
+++ MCB-project/Code/HERO/src/main.cpp
@@ -9,17 +9,18 @@ tap::algorithms::SmoothPid pidController = tap::algorithms::SmoothPid(pid_conf_d
 tap::arch::PeriodicMilliTimer sendMotorTimeout(2);
 src::Drivers *drivers;
 int indexerDesiredRPM = 0;
-int indexerMaxRPM = 10000;
-int indexerStepSpeed = 1000;
+int indexerMaxRPM = 1000;
+int indexerStepSpeed = 10;
+bool alreadyChanged = false;
 
 int main() {
     src::Drivers *drivers = src::DoNotUse_getDrivers();
     Board::initialize();
     drivers->can.initialize();
     drivers->remote.initialize();
-    tap::motor::DjiMotor indexer = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, true, "ID1", 0, 0);
-    tap::motor::DjiMotor flywheel_one = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, true, "ID1", 0, 0);
-    tap::motor::DjiMotor flywheel_two = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR1, tap::can::CanBus::CAN_BUS1, true, "ID1", 0, 0);
+    tap::motor::DjiMotor indexer = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR7, tap::can::CanBus::CAN_BUS2, true, "ID1", 0, 0);
+    tap::motor::DjiMotor flywheel_one = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR5, tap::can::CanBus::CAN_BUS2, true, "ID1", 0, 0);
+    tap::motor::DjiMotor flywheel_two = tap::motor::DjiMotor(src::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR8, tap::can::CanBus::CAN_BUS2, true, "ID1", 0, 0);
     indexer.initialize();
     flywheel_one.initialize();
     flywheel_two.initialize();
@@ -31,10 +32,23 @@ int main() {
         if(drivers->remote.isConnected()) { //Doing stuff for the remote every loop   
             if(drivers->remote.getSwitch(drivers->remote.Switch::LEFT_SWITCH) == drivers->remote.SwitchState::MID) { 
                 //Do Nothing
+                alreadyChanged = false;
       	    } else if((drivers->remote.getSwitch(drivers->remote.Switch::LEFT_SWITCH) == drivers->remote.SwitchState::UP)) { 
                 //Increment the Indexer Speed
+                if(!alreadyChanged) {
+                    if(indexerDesiredRPM<indexerMaxRPM) {
+                        indexerDesiredRPM+=indexerStepSpeed;
+                    }
+                    alreadyChanged = true;
+                }
             } else if(drivers->remote.getSwitch(drivers->remote.Switch::LEFT_SWITCH) == drivers->remote.SwitchState::DOWN) { 
                 //Decrement the Indexer
+                if(!alreadyChanged) {
+                    if(-indexerDesiredRPM<indexerMaxRPM) {
+                        indexerDesiredRPM-=indexerStepSpeed;
+                    }
+                    alreadyChanged = true;
+                }
             }
             if(sendMotorTimeout.execute()) {
                 pidController.runControllerDerivateError(indexerDesiredRPM - indexer.getShaftRPM(), 1);
@@ -43,6 +57,8 @@ int main() {
         } else { //Remote not connected, so have everything turn off (Saftey features!)
             
         }
+        drivers->djiMotorTxHandler.encodeAndSendCanData(); //Processes these motor speed changes into can signal
+
     }
     return 0;
 }
